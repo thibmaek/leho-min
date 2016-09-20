2ee02049e3071d9b7e4c293ca782becbb4d72f95
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-09-20 23:59:49 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+(() => {
+  document.querySelector('.portal_stip_wrapper').remove();
+  document.getElementById('corps').querySelector('.portal_div_wrapper').remove();
+
+  document.getElementById('PracticalInfo').parentNode.style.width = "100%";
+  document.getElementById('PracticalInfo').parentNode.style.margin = 0;
+  document.getElementById('PracticalInfo').style.width = "100%";
+})();
