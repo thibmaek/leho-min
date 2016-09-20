4e06828ed01f5b1b3f3aa2e110156b40bd8a6776
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-09-21 00:38:50 +0200

    Add a settings button and default black theme

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 (() => {
-  document.querySelector('.portal_stip_wrapper').remove();
-  document.getElementById('corps').querySelector('.portal_div_wrapper').remove();
+	const domCheck = node => {
+		node ? node.remove() : console.log('node doesnt exist');
+	};
+
+  domCheck(document.querySelector('.portal_stip_wrapper'));
+  domCheck(document.getElementById('corps').querySelector('.portal_div_wrapper'));
+  domCheck(document.getElementById('breadcrumbs'));
 
   document.getElementById('PracticalInfo').parentNode.style.width = "100%";
   document.getElementById('PracticalInfo').parentNode.style.margin = 0;
   document.getElementById('PracticalInfo').style.width = "100%";
+
+	const settings = document.createElement('li');
+	settings.innerHTML = "<a>leho-min settings</a>";
+	document.querySelector('#usermenu ul').appendChild(settings);
 })();
