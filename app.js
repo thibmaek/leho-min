fc1f86dac61147a5361b1115c8e290a9ddd6ad42
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-09-21 15:54:02 +0200

    Remove some courses, update icon

--- app.js
+++ app.js
@@ -6,6 +6,11 @@
   domCheck(document.querySelector('.portal_stip_wrapper'));
   domCheck(document.getElementById('corps').querySelector('.portal_div_wrapper'));
   domCheck(document.getElementById('breadcrumbs'));
+  domCheck(document.getElementById('course_ALGAUTOSTUDSSBKa7ae'));
+  domCheck(document.getElementById('course_ALGAUTOSTUDENQUETES50ca'));
+  domCheck(document.getElementById('course_ALGAUTOSTUDJHNYP9b60'));
+  domCheck(document.getElementById('course_ALGAUTOSTUDLEHO54f7'));
+  domCheck(document.getElementById('course_ALGAUTOSTUDSTUDRAADf334'));
 
   document.getElementById('PracticalInfo').parentNode.style.width = "100%";
   document.getElementById('PracticalInfo').parentNode.style.margin = 0;
