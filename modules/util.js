c907eeb7859c9580a96723094d9ec9b562c064ec
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-09-21 22:00:19 +0200

    Prepera beta release

--- modules/util.js
+++ modules/util.js
@@ -1,27 +1,22 @@
-export const $ = selector => {
-
+export default $ = selector => {
   let result;
 
-  if(selector === 'body'){
+  if(selector === 'body') {
     return document.body;
-  }else if(selector === 'head'){
+  } else if(selector === 'head') {
     return document.head;
-  }else if(/^[\#.]?[\w-]+$/.test(selector)){
+  } else if(/^[\#.]?[\w-]+$/.test(selector)) {
 
-    if(selector[0] === '#'){
+    if(selector[0] === '#') {
       return document.getElementById(selector.slice(1));
-    }else if(selector[0] === '.'){
+    } else if(selector[0] === '.') {
       result = document.getElementsByClassName(selector.slice(1));
-    }else{
-      result = document.getElementsByTagName(selector);
-    }
-  }else{
-    result = document.querySelectorAll(selector);
-  }
+    } else{ result = document.getElementsByTagName(selector);}
+
+  } else {result = document.querySelectorAll(selector);}
 
 
   let elements = [...result];
   if(elements.length === 1) return elements[0];
   return elements;
-
 };
